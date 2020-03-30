8966314b099c37223a55ad36009d852d52e17592
tintinmar1995 <martin.masson@protonmail.com>
2020-03-30 20:05:37 +0200

    Add credit and privacy modal

--- assets/js/modal.js
+++ assets/js/modal.js
@@ -7,7 +7,6 @@ var span = document.getElementsByClassName("close")[0];
 function openModal() {
   modal.style.display = "block";
 }
-
 // window.onload = openModal;
 
 // When the user clicks on <span> (x), close the modal
@@ -19,5 +18,32 @@ span.onclick = function() {
 window.onclick = function(event) {
   if (event.target == modal) {
     modal.style.display = "none";
+  }
+}
+
+//////////////////////////////////////
+
+var link_credit = document.getElementById("open-credits");
+
+// Get the button that opens the modal
+var modal_credit = document.getElementById("credits");
+
+// Get the <span> element that closes the modal
+var span_credit = document.getElementById("close-credit");
+
+// When the user clicks on the button, open the modal
+link_credit.onclick = function() {
+  modal_credit.style.display = "block";
+}
+
+// When the user clicks on <span> (x), close the modal
+span_credit.onclick = function() {
+  modal_credit.style.display = "none";
+}
+
+// When the user clicks anywhere outside of the modal, close it
+window.onclick = function(event) {
+  if (event.target == modal_credit) {
+    modal_credit.style.display = "none";
   }
 }
