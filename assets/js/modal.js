dcda808f0f2c7238cb882290f85d3b85b698e990
tintinmar1995 <martin.masson@protonmail.com>
2020-05-10 14:12:06 +0200

    Clean modal.js code

--- assets/js/modal.js
+++ assets/js/modal.js
@@ -1,48 +1,31 @@
-// Get the modal
+function openModal(one_modal) {
+  one_modal.style.display = "block";
+  document.documentElement.style.overflow = 'hidden';
+  document.body.scroll = "no";
+}
+
+function closeModal(one_modal) {
+  one_modal.style.display = "none";
+  document.documentElement.style.overflow = 'scroll';
+  document.body.scroll = "yes";
+}
+
+// Warning modal
 var modal = document.getElementById("myModal");
-
-// Get the <span> element that closes the modal
 var span = document.getElementsByClassName("close")[0];
-
-function openModal() {
-  modal.style.display = "block";
-}
-
-// When the user clicks on <span> (x), close the modal
-span.onclick = function() {
-  modal.style.display = "none";
-}
-
-// When the user clicks anywhere outside of the modal, close it
-window.onclick = function(event) {
-  if (event.target == modal) {
-    modal.style.display = "none";
-  }
-}
-
-//////////////////////////////////////
-
+// Credit modal
 var link_credit = document.getElementById("open-credits");
-
-// Get the button that opens the modal
 var modal_credit = document.getElementById("credits");
-
-// Get the <span> element that closes the modal
 var span_credit = document.getElementById("close-credit");
 
-// When the user clicks on the button, open the modal
-link_credit.onclick = function() {
-  modal_credit.style.display = "block";
-}
-
-// When the user clicks on <span> (x), close the modal
-span_credit.onclick = function() {
-  modal_credit.style.display = "none";
-}
+// setup actions
+link_credit.onclick = function(){openModal(modal_credit)}
+span.onclick = function(){closeModal(modal)}
+span_credit.onclick = function(){closeModal(modal_credit)}
 
 // When the user clicks anywhere outside of the modal, close it
 window.onclick = function(event) {
-  if (event.target == modal_credit) {
-    modal_credit.style.display = "none";
+  if (event.target == modal || event.target == modal_credit) {
+    closeModal(event.target)
   }
 }
