02837a3c76ff5bd4de3dff8c7860960132539519
Martin M <martin.masson@datastorm.fr>
2023-03-13 19:12:26 +0100

    CLEAN: Split index.js in two files

--- assets/js/index_routing.js
+++ assets/js/index_routing.js
@@ -1,4 +1,4 @@
-// TODO: Transform this to a component
+
 const retrieveOrderInfo = function(idCmd){
   if(idCmd != null){
     $.get("https://api.champ-ramard.fr/v2/public/status.php?id_cmd="+idCmd, function(result){
@@ -39,55 +39,14 @@ const retrieveOrderInfo = function(idCmd){
   }
 };
 
-function updateSettings() {
-  const mapping_size = {'aspb': "1kg", 'aspv': "1kg", 'fraise': "500g"}
-
-  for (key of ['aspb', 'aspv', 'fraise']) {
-    elem = document.getElementById(key)
-    settings[key] ? elem.enable() : elem.disable();
-    elem.setUnitPrice(settings[key + '_price']);
-    elem.setUnitSize(mapping_size[key]);
-  }
-  for (key in settings){
-    if (key == 'website_title'){
-      document.getElementsByClassName("header-headline")[0].innerText = settings[key];
-    }
-    if (key == 'website_subtitle'){
-      document.getElementsByClassName("header-running-text")[0].innerText = settings[key];
-    }
-  }
-}
-
 
 // REDIRECT TO THANK YOU PAGE
 const urlParams = new URLSearchParams(window.location.search);
-idCmd = urlParams.get("id-cmd");
+let idCmd = urlParams.get("id-cmd");
 retrieveOrderInfo(idCmd);
 document.getElementById("btn-id-cmd").onclick = function(){
   retrieveOrderInfo(document.getElementById("id-cmd").value)
 };
-
-
-// Add behaviour when change size of each product
-for (ipt of ["aspb", "aspv", "fraise"]){
-  document.getElementById(ipt).onChange(() => {
-    let price = computeBill({
-      'aspb': document.getElementById("aspb").getTotalPrice(),
-      'aspv': document.getElementById("aspv").getTotalPrice(),
-      'fraise': document.getElementById("fraise").getTotalPrice(),
-    })
-
-    if(price > 0){
-      document.getElementById("consigne-order").style = "display:none;"
-      document.getElementById("continue-order").style = "display:block;"
-      document.getElementById("continue-order").innerHTML = "<h5>Poursuivre la commande ("+price+"€)</h5>"
-    }else{
-      document.getElementById("consigne-order").style = "display:block;"
-      document.getElementById("continue-order").style = "display:none;"
-    }
-  });
-}
-
 
 // REDIRECT TO ERROR PAGE
 if(urlParams.get("error")!=null){
